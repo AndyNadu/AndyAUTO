3b16fe9a8d1cbbbcbea5721bf7b07b33bd5bc7ee
AndyNadu <andy.nadu01@gmail.com>
2024-02-03 01:00:11 +0200

    created the home page

--- DealerAutoFrontend/src/app/Custom Components/navigation-bar/navigation-bar.component.ts
+++ DealerAutoFrontend/src/app/Custom Components/navigation-bar/navigation-bar.component.ts
@@ -1,21 +1,29 @@
 import { Component } from '@angular/core';
 
+import { MatCardModule } from '@angular/material/card';
 import { MatIconModule } from '@angular/material/icon';
 import { MatButtonModule } from '@angular/material/button';
 import { MatToolbarModule } from '@angular/material/toolbar';
+
 
 
 @Component({
   selector: 'app-navigation-bar',
   standalone: true,
   imports: [
-    MatToolbarModule,
+    MatCardModule,
     MatIconModule,
-    MatButtonModule
+    MatButtonModule,
+    MatToolbarModule
   ],
   templateUrl: './navigation-bar.component.html',
   styleUrl: './navigation-bar.component.css'
 })
 export class NavigationBarComponent {
-
+  scrollDown() {
+    const element = document.getElementById('buy-a-car');
+    if (element) {
+      element.scrollIntoView({ behavior: 'smooth', block: 'start' });
+    }
+  }
 }
--- DealerAutoFrontend/src/app/app.component.ts
+++ DealerAutoFrontend/src/app/app.component.ts
@@ -1,16 +1,23 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
+import { MatIconModule } from '@angular/material/icon';
+import { MatToolbarModule } from '@angular/material/toolbar';
+
 import { NavigationBarComponent } from './Custom Components/navigation-bar/navigation-bar.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [RouterOutlet,
-            NavigationBarComponent],
+    MatIconModule,
+    MatToolbarModule,
+    NavigationBarComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'DealerAutoFrontend';
+
+
 }
