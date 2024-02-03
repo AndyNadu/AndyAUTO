d4eb8da8b9a87a22934c50b6381c2df9984329d6
AndyNadu <andy.nadu01@gmail.com>
2024-02-03 09:32:22 +0200

    Hardcoded some car-filters

--- DealerAutoFrontend/src/app/app.component.ts
+++ DealerAutoFrontend/src/app/app.component.ts
@@ -2,22 +2,33 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+import { MatSelectModule } from '@angular/material/select';
 import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { FormControl, FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 import { NavigationBarComponent } from './Custom Components/navigation-bar/navigation-bar.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet,
+  imports: [
+    RouterOutlet,
     MatIconModule,
+    FormsModule,  
+    ReactiveFormsModule,
+    MatButtonModule,
+    MatSelectModule,
     MatToolbarModule,
+    MatFormFieldModule,
     NavigationBarComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'DealerAutoFrontend';
-
+  toppings = new FormControl('');
+  toppingList: string[] = ['Extra cheese', 'Mushroom', 'Onion', 'Pepperoni', 'Sausage', 'Tomato'];
 
 }
